3a867b9b7673515893233a3aa38c2af0dafc27fb
Dylan Hall <dylan@mathspathway.com>
2023-01-09 17:13:08 +1000

    initial commit

---
+++ FilesToDelete.cs
@@ -0,0 +1,99 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ClearTempFiles;
+
+public class FilesToDelete
+{
+    public FilesToDelete()
+    {
+        FileMask = string.Empty;
+        Path = string.Empty;
+    }
+
+    public FilesToDelete(string path, FilesToDelete parent)
+    {
+        RetentionInDays = parent.RetentionInDays;
+        FileMask = parent.FileMask;
+        Recurse = true;
+        Path = path;
+    }
+
+    public int RetentionInDays { get; set; }
+    public string FileMask { get; set; }
+    public bool Recurse { get; set; }
+    public string Path { get; set; }
+
+    
+}
+
+public static class FilesToDeleteMethods
+{
+
+    public static Task ClearFolder(this FilesToDelete f, DateTimeOffset? now = null) =>
+        ClearFolder(f.Path, f.Recurse, isSubFolder: false, f.RetentionInDays, f.FileMask, now ?? DateTimeOffset.UtcNow);
+
+    public static Task ClearFolder(string path, bool recurse, bool isSubFolder, int retentionInDays, string fileMask,
+        DateTimeOffset now)
+    {
+        var tasks = new List<Task>();
+        try
+        {
+            if (string.IsNullOrEmpty(path) || !Directory.Exists(path)) return Task.CompletedTask;
+
+            if (recurse)
+            {
+                tasks.AddRange(Directory.GetDirectories(path).Select(subPath =>
+                    ClearFolder(subPath, true, true, retentionInDays, fileMask, now)));
+            }
+
+            var files = Directory.GetFiles(path);
+
+            if (retentionInDays > 0)
+            {
+                var cutOff = now - TimeSpan.FromDays(retentionInDays);
+                files = files.Where(f => new FileInfo(f).LastWriteTimeUtc < cutOff).ToArray();
+            }
+
+            if (!string.IsNullOrEmpty(fileMask))
+            {
+                var regex = new Regex(fileMask, RegexOptions.IgnoreCase);
+                files = files.Where(f => regex.IsMatch(f)).ToArray();
+            }
+
+            tasks.Add(Task.Run(() =>
+            {
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error deleting file: {file}, {e.Message}");
+                    }
+                }
+            }));
+
+            return isSubFolder
+                ? Task.WhenAll(tasks).ContinueWith(r =>
+                {
+                    try
+                    {
+                        var hasFilesOrFolders = Directory.EnumerateFiles(path).Any() || Directory.EnumerateDirectories(path).Any();
+                        if (!hasFilesOrFolders) Directory.Delete(path);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error removing {path}: {ex.Message}");
+                    }
+                }, TaskContinuationOptions.OnlyOnRanToCompletion)
+                : Task.WhenAll(tasks);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error cleaning {path}: {ex.Message}");
+            return tasks.Any() ? Task.WhenAll(tasks) : Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Text.Json;
+using ClearTempFiles;
+
+const string configFile = "files-to-delete.json";
+var configPath = $"{AppContext.BaseDirectory}\\{configFile}";
+if (File.Exists(configPath))
+{
+    await using (var stream = File.OpenRead(configPath))
+    {
+        var config = await JsonSerializer.DeserializeAsync<List<FilesToDelete>>(stream);
+        if (config == null) throw new Exception("Config not correct");
+     
+        await Task.WhenAll(config.Select(f => f.ClearFolder()));
+        
+        return;
+    }
+}
+
+var example = new List<FilesToDelete>
+{
+    new FilesToDelete
+    {
+        FileMask = @".*\.zip",
+        Path = @"C:\temp-zips",
+        Recurse = false,
+        RetentionInDays = 2
+    },
+    new FilesToDelete
+    {
+        Path = @"C:\temp",
+        Recurse = true
+    },
+};
+
+await File.WriteAllTextAsync(configPath, JsonSerializer.Serialize(example));
+
